bd9e0eb0db554ef60eae07800fece732db415765
Jorge Luis Martinez Ricardo <jorgemartinezricardo@gmail.com>
2024-09-08 21:21:59 -0500

    agreagar todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ //  componente
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'bases-angular-primeros-pasos';
+export class AppComponent { // esto es un modulo que se exporta
+                            // con el conjunto de componente
+  public title:string = 'Primera app angular';
+  public descripcion:string = 'Estoy aprendiendo angular xd ';
+
+  mostrarNombre(nombre: string): string{
+    return nombre;
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TareasComponentComponent } from './tareas-component/tareas-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TareasComponentComponent,
+  
   ],
   imports: [
     BrowserModule,
